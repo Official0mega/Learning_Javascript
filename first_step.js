29052f374d22b14cbed4f9c9219b31b40763b720
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 09:49:36 +0100

    first session

---
+++ first_step.js
@@ -0,0 +1,22 @@
+//Datatypes and Variables
+//
+// 7 Types of Datatypes used in Javascript
+// 1. Undefined = That has not been defined.
+// 2. Null = it means Nothing.
+// 3. Boolean = This is Either True or False
+// 4. Object = Can store a lot of key value pairs.
+// 5. String = Set of Characters in double or single Quotes and can also be both numbers and characters mixed.
+// 6. Symbol : 
+// 7. Number = Can be either Decimal or whole numbers.
+//
+
+// Types of Declaration
+
+var myName = "Tony" //Can be changed
+
+myName = 8
+
+
+let ourName = "David James Taiye" //Used within a scope
+
+const pi = 3.14 // This can never be changed and once altered, it gives an error output.
