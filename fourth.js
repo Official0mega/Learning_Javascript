332448ace07467f0a92dd3f3871177122f8e2038
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 10:19:23 +0100

    4th session

---
+++ fourth.js
@@ -0,0 +1,18 @@
+// Letter Case Sensitivity
+
+// Assignment of Variables
+
+var studyCaseVar;
+var properBiroFiles;
+var titleCaseCover;
+
+// Declaration, first alphabet must be in Lowercase,then the following next word(if there's any)
+
+// Declaring of Variables
+studyCaseVar = 10;
+properBiroFiles = 25;
+titleCaseCover = 100;
+
+var result = titleCaseCover + properBiroFiles + studyCaseVar;
+console.log(result)
+
