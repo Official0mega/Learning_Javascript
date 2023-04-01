548c3879acf0b1ec137fe80bff9dcd38dc086203
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 13:47:44 +0100

    simple calculations in Javascript

---
+++ calc.js
@@ -0,0 +1,82 @@
+// Addition of Numbers(Sum +)
+
+var slice = 50;
+var fain = 17;
+var vi = fain + slice;
+
+console.log(vi)
+
+// Subtraction of Numbers(Difference -)
+
+var bet = 400;
+var con = 488;
+var get = bet - con;
+
+console.log(get)
+
+// Multiplication of Numbers(Product *)
+
+var hi = 230;
+var pi = 50;
+var jp = hi * pi;
+
+console.log(jp)
+
+// Division of Numbers(/)
+
+var com = 60;
+var toy = 5;
+var rit = com / toy;
+
+console.log(rit)
+
+// Incrementing Numbers(++)
+
+var hive = 82;
+hive ++;
+
+console.log(hive)
+
+
+// Decrementing Numbers (--)
+
+var heep = 10;
+heep --;
+
+console.log(heep)
+
+
+// Decimal Numbers (.)
+
+var gnu = 300;
+var cpu = 50;
+var hard = gnu / cpu;
+
+console.log(hard)
+
+
+// Multiplying of Decimals (.*./)
+
+var hyp = 10.89;
+var gut = 2.31;
+var jet = hyp * gut;
+
+console.log(jet)
+
+
+// Dividing Decimals (././)
+
+var wat = 48.7;
+var rat = 10.5;
+var cat = wat / rat;
+
+console.log(cat)
+
+
+// Finding a Remainder(Modulus/(%)Remainder)
+
+var tuet = 57;
+var hood = 14;
+var stack = tuet % hood;
+
+console.log(stack)
