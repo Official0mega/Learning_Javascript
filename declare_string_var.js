eeb0ce9e74f8e828bcab65c28fd63aaad68ed5b7
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 15:23:35 +0100

    6th session

---
+++ declare_string_var.js
@@ -0,0 +1,44 @@
+// Declaring String Variables
+//
+
+var myFirstName = "Timothy";
+
+console.log(myFirstName)
+
+
+
+// Escaping Literal Quotes in Strings
+//
+
+var myStr = "I am a \"double quoted \"string inside a\" double quotes";
+
+console.log(myStr)
+
+
+// Quoting Strings with Single Quotes.
+//
+
+var myString = 'Am a "Busy Person" at all times';
+
+console.log(myString)
+
+
+// Escaping Sequences in Strings
+// 1. /*****
+// 2. CODE OUTPUT
+// 3. \' Single quote
+// 4. \" Double quote
+// 5. \\ Backslash
+// 6. \n New line
+// 7. \r Carriage return
+// 8. \t Tab
+// 9. \b Backspace
+// 10. \f Form feed
+// 11. *****/
+//
+
+
+var myStrings = "Firstline\n\t\\SecondLine\nThirdLine";
+console.log(myStrings)
+
+
