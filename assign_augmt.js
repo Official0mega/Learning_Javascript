5fd3ead7d945b72bd8c83ebaa34f73e817ef9f1f
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 14:51:49 +0100

    5th session

---
+++ assign_augmt.js
@@ -0,0 +1,80 @@
+var a = 5;
+var b = 10;
+var c = 15;
+
+// Compound Assignment with Augment Addition
+//
+
+a += 15;
+b += 10;
+c += 5;
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+// Compound Assignment with Augment Subtraction
+//
+
+var c = 4;
+var d = 10;
+var e = 7;
+
+// do not touch this line
+
+c -= 6;
+d -= 10;
+e -= 9;
+
+console.log(c)
+console.log(d)
+console.log(e)
+
+
+// Compound Assignment with Augment Multiplication
+//
+
+
+var f = 100;
+var g = 42;
+var h = 29;
+
+// Do not touch this line
+
+
+f *= 5;
+g *= 10;
+h *= 11;
+
+console.log(f)
+console.log(g)
+console.log(h)
+
+
+// Compound Assignment with Augment Division
+
+
+var i = 60;
+var j = 100;
+var k = 90;
+
+// Do not touch this line
+//
+
+
+i /= 10;
+j /= 10;
+k /= 3;
+
+console.log(i)
+console.log(j)
+console.log(k)
+
+
+
+
+
+
+// Compound Assignment with Augment Addition
+//
