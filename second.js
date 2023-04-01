3c05ff508b0114850e9893f7a38d43a3f4ec8247
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 09:58:34 +0100

    Session 2

---
+++ second.js
@@ -0,0 +1,14 @@
+//Assigning VS Declaring a function
+//
+
+var a; // Declaring a function
+
+var b = 2; // Assigning and Declaring a function.
+
+console.log(a)
+
+a = 7; // Declaring a function
+
+b = a; // Declaring a function
+
+console.log(b)
