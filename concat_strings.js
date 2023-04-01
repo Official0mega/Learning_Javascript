19f0dec12e330c3cb51e4699907a7b5114c52cb7
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 19:19:37 +0100

    7th Session Concatenating Strings

--- concat_strings.js
+++ concat_strings.js
@@ -1,7 +1,7 @@
 // Concatenating Strings with Plus (+) Operator.
 //
 
-var myStr = "This is the first Statementi made, " + "This is the Second Statement that i also made."
+var myStr = "This is the first Statementi made, " + "This is the Second Statement that i also made.\n"
 
 console.log(myStr)
 
@@ -10,8 +10,28 @@ console.log(myStr)
 
 var testFiles = "First comment line of Actions, "
 
-testFiles += "This is the second line of Action made."
+testFiles += "This is the second line of Action made.\n"
 
 console.log(testFiles)
 
+
+// Concatenating Strings with Variables.
+//
+
+var myName = "David James Taiye"
+
+var runGhost = "The Author of this Javascript Program is " + myName + " and he has been doing his Best."
+
+console.log(runGhost)
+
+
+// Appending Variables to String
+//
+
+var myStrings = "The Best Programmer to ever Lived";
+
+var setStr = "I would love to be ";
+
+setStr += myStrings
+console.log(setStr)
 //The end
