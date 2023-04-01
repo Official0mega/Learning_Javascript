b3de6ee199d19083fff1a510e11d47c6f85823f0
Official0mega <supr3m30m3ga@gmail.com>
2023-04-01 19:02:53 +0100

    7th session concatenating strings

---
+++ concat_strings.js
@@ -0,0 +1,17 @@
+// Concatenating Strings with Plus (+) Operator.
+//
+
+var myStr = "This is the first Statementi made, " + "This is the Second Statement that i also made."
+
+console.log(myStr)
+
+// Concatenating Strings with Plus Equals Operator.
+//
+
+var testFiles = "First comment line of Actions, "
+
+testFiles += "This is the second line of Action made."
+
+console.log(testFiles)
+
+//The end
